0c31c95b464d9de56fe2fe76ddfc67d8d090ce36
singh17shikha <shikha456singh@gmail.com>
2023-05-11 03:02:43 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Profilecard from './Profilecard';
+import Scrolbar from './Scrolbar';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+return (
+<div className={styles.container}>
+<Scrolbar>
+<Profilecard 
+       name="John Doe"
+       image="https://via.placeholder.com/150"
+       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+     />
+     <Profilecard 
+       name="Jane Doe"
+       image="https://via.placeholder.com/150"
+       description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+     />
+<Profilecard 
+       name="Jack Smith"
+       image="https://via.placeholder.com/150"
+       description="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+     />
+</Scrolbar>
+</div>
+);
 }
 
 export default App;
